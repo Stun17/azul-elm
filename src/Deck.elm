5f13634eaf0addee85e9d979767d6f991ce45db1
Your Name <you@example.com>
2017-12-07 14:18:21 +0000

    with community package

--- src/Deck.elm
+++ src/Deck.elm
@@ -4,37 +4,46 @@ import Random
 import Html.Attributes exposing (style)
 
 main = Html.program
-    { init            = ( [(0 , 0)] , Cmd.none)
+    { init            = ( [] , Cmd.none)
     , view            = view
     , update          = update
     , subscriptions   = subscriptions
     }
 
-type Msg = Shuffle | Face (List (Int , Int))
+type Msg = Shuffle | Face (List Int)
 
 
-subscriptions : List (Int , Int) -> Sub Msg
+subscriptions : List Int -> Sub Msg
 subscriptions m = Sub.none
 
     
-update : Msg -> List (Int , Int) -> (List (Int , Int) ,  Cmd Msg)
+update : Msg -> List Int -> (List Int ,  Cmd Msg)
 update b m = case b of
     Shuffle ->
         (m , Random.generate Face myfunc)
     Face n  ->
-        (n , Cmd.none)
+        (hfun2 n , Cmd.none)
 
-view : List (Int , Int) -> Html Msg
+view : List Int -> Html Msg
 view xs  =
   div []
-    [ span [style [("font-size", "24pt") , ("background" , "lime")]] (himfunc xs)
+    [ p [style [("font-size", "24pt")]] (hfun1 xs)
     , p [] []      
     , button [ onClick Shuffle ] [ text "Shuffle" ]
     ]
 
-myfunc : Random.Generator (List (Int , Int))
-myfunc = Random.list 52 (Random.pair (Random.int 1 4) (Random.int 1 13))
+myfunc : Random.Generator (List Int)
+myfunc = Random.list 512 (Random.int 1 52)
 
+hfun1 : List Int -> List (Html a)
+hfun1 xs = List.map (\x -> text (" " ++ (toString x))) xs
+
+hfun2 : List Int -> List Int
+hfun2 xs = List.filter (\x -> if List.member x (Listxs then 0 else x) xs
+
+
+
+{-                  
 himfunc : List (Int , Int) -> List (Html m)          
 himfunc xs =
     List.map
@@ -62,3 +71,4 @@ himfunc xs =
                      _  -> " "
           in span [] [text (" " ++ r1 ++ s1 ++ " ")]
       ) xs
+ -}
