482a14d1edd254accb3bff5d144628ee083f35dc
Your Name <you@example.com>
2017-12-08 11:02:24 +0000

    with buttons now

--- src/Main.elm
+++ src/Main.elm
@@ -16,23 +16,32 @@ main = Html.program
     }
 
 init : (Struc, Cmd msg)    
-init = ({ roll_c = 200 , roll_p = 200 , deck = []} , Cmd.none)
+init = ({ roll_c = 200 , roll_p = 200 , pot = 0 , deck = []} , Cmd.none)
        
 subscriptions : Struc -> Sub Msg
 subscriptions m = Sub.none
 
-type alias Struc = { roll_c : Int , roll_p : Int , deck : List Int }
+type alias Struc =
+    { roll_c : Int
+    , roll_p : Int
+    , pot : Int
+    , deck : List Int
+    }
     
-type Msg = Shuffle | Show (List Int)
+type Msg = Shuffle | Show (List Int) | Bet | AllIn
                   
 update : Msg -> Struc -> (Struc , Cmd Msg)
-update b m =
-  let hfun0 = Random.list 2048 (Random.int 2 53)
-  in case b of
+update b m = case b of
+    AllIn ->
+        if m.roll_p > m.roll_c
+        then ({ m | pot = m.pot + m.roll_c , roll_p = m.roll_p - m.roll_c } , Cmd.none)
+        else ({ m | pot = m.pot + m.roll_p , roll_p = 0 } , Cmd.none)    
+    Bet ->
+        ({m | pot = m.pot + 1 , roll_p = m.roll_p - 1 } , Cmd.none)
     Shuffle ->
-        (m , Random.generate Show hfun0)
+        (m , Random.generate Show (Random.list 2048 (Random.int 2 53)))
     Show m ->
-        ({ roll_c = 200 , roll_p = 200 ,  deck = List.Extra.unique m } , Cmd.none)
+        ({ roll_c = 200 , roll_p = 200 , pot = 0 , deck = List.Extra.unique m } , Cmd.none)
 
 view : Struc -> Html Msg
 view m =
@@ -46,36 +55,45 @@ view m =
       xf3 = List.head (List.drop 6 xs)
       xt  = List.head (List.drop 7 xs)
       xr  = List.head (List.drop 8 xs)
-      bstyle = [("width","70px"), ("margin-left","100px")]          
+      bstyle = [("width","70px") , ("margin-left","100px")]
+      mstyle = [("font-size","26pt") , ("color","yellow"), ("align","left")
+               , ("width","20%")
+               ]
    in
       body
          [ style [("font-family","mono") , ("background","green")] ]
          [ br [] []
-         , button [ onClick Shuffle  , style bstyle] [ text " Deal " ]
+         , button [ onClick Shuffle , style bstyle] [ text " Deal " ]
          , p [] [] , hr [] [] , br [] []
 
          , div [style [("margin-left", "400px")]]
              [    
-               table [style [("width","45%")]]
+               table [style [("width","55%")]]
                  [    tr [ style [("height","120px")]]
-                         (List.append ( List.map hfun2 [ x11 , x12 ] )
-                              [td [] [text (toString m.roll_c )]]
+                         (List.append
+                              [td [style mstyle] [text (toString m.roll_c )]]
+                              ( List.map hfun2 [ x11 , x12 ] ))
                     , tr [ style [("rowspan","5"), ("height","100px")]] []
                     , tr [ style [("height","120px")]]
-                         ( List.map hfun2 [ xf1 , xf2 , xf3 , xt , xr ] )
+                         (List.append
+                              [ td [style mstyle] [text (toString m.pot )] ]
+                              ( List.map hfun2 [ xf1 , xf2 , xf3 , xt , xr ] ) )
                     , tr [ style [("rowspan","5"), ("height","100px")]] []          
                     , tr [ style [("height","120px")]]
-                         ( List.map hfun2 [ x21 , x22 ] )
+                         ( List.append
+                               [ td [style mstyle] [text (toString m.roll_p )] ]
+                               ( List.map hfun2 [ x21 , x22 ] ))
                  ]
              ]
 
          , p [] []  , hr [] []  , br [] []
          , button [  style bstyle]  [ text " Fold  " ] 
-         , button [  style bstyle]  [ text " Check " ] 
-         , button [  style bstyle]  [ text " Bet   " ] 
+         , button [  style bstyle]  [ text " Check " ]
+         , button [  style bstyle]  [ text " Call " ]              
+         , button [ onClick Bet ,  style bstyle]  [ text " Bet   " ] 
          , button [  style bstyle]  [ text " Raise " ] 
          , input  [  style [("width","100px")]  ]  [ text "100"]      
-         , button [  style bstyle] [ text " All-In " ]
+         , button [ onClick AllIn , style bstyle] [ text " All-In " ]
 
          , br [] [] , hr [] []    
          , p [] [text "demo ver 0.0"]
@@ -96,7 +114,7 @@ hfun2 k0 =
                    ]
   in
       case k0 of
-        Nothing          -> ty "white" "" ""
+        Nothing          -> ty "green" "" ""
         Just k1          -> case (Dict.get k1 myhash52) of
            Just (k2,k3)  ->   case (Dict.get k2 myhashSuit) of
              Just suit   ->     case (Dict.get k3 myhashRank) of
@@ -105,8 +123,8 @@ hfun2 k0 =
                                     2 -> ty "black" rank suit
                                     3 -> ty "red"   rank suit
                                     4 -> ty "red"   rank suit
-                                    _ -> ty "white" "" ""
-               Nothing   ->              ty "white" "" ""
-             Nothing     ->              ty "white" "" ""
-           Nothing       ->              ty "white" "" ""
+                                    _ -> ty "green" "" ""
+               Nothing   ->              ty "green" "" ""
+             Nothing     ->              ty "green" "" ""
+           Nothing       ->              ty "green" "" ""
            
