630b8ece7a02eb270a52a522b0cc86977bdb3822
Your Name <you@example.com>
2017-12-10 18:18:33 +0000

    done till trades start

--- src/Main.elm
+++ src/Main.elm
@@ -22,100 +22,96 @@ main = Html.program
 -----  Model
 
 init : (Struc, Cmd msg)
-init = ({ ppocket = [(0,0)], pstack = 200, pstatus = Id, pdeal = False
-        , kpocket = [(0,0)], kstack = 200, kstatus = Id, kdeal = False
-        , pot = 0, board = [(0,0)], hand = 0, order = True, bet = 3, status = St
+init = ({ ppocket = [(0,0), (0,0)], pstack = 200, pstage = Id, pdeal = False
+        , kpocket = [(0,0), (0,0)], kstack = 200, kstage = Id, kdeal = False
+        , pot = 0, board = [(0,0), (0,0), (0,0), (0,0), (0,0)]
+        , hand = 0, order = True, bet = 3, status = St
         } , Cmd.none)
 
 subscriptions : Struc -> Sub Msg
 subscriptions m = Sub.none
 
-type PStatus =  Ca | Ch | Fo | Al | Be | Id | Wi
-type GStatus =  St | Dc | Pr | Fl | Tn | Rv
+type Pstage =  Ca | Ch | Fo | Al | Be | Id | Wi 
     
+type GStatus =  St | Dc | Pr | Fl | Tn | Rv
+
 type alias Struc =
-    { ppocket  : List (Int , Int) 
+    { ppocket  : List (Int , Int)
     , kpocket  : List (Int , Int )
+    , board    : List (Int , Int)                 
     , pstack   : Int
     , kstack   : Int
     , pot      : Int
     , pdeal    : Bool      -- who is Small Blind and dealer now ?
     , kdeal    : Bool      -- who is Small Blind and dealer now ?
-    , order    : Bool      -- who is thinking now ?       
-    , pstatus  : PStatus
-    , kstatus  : PStatus
-    , status   : GStatus          
+    , order    : Bool      -- who is thinking now ?
+    , pstage   : Pstage
+    , kstage   : Pstage
+    , status   : GStatus
     , hand     : Int
-    , board    : List (Int , Int)
-    , bet      : Int             
+    , bet      : Int
     }
-    
+
 type Msg = Start
          | Shuffle
          | Initial (List Int)
-         | Hand (List Int)
+         | Hand    (List Int)
          | AllIn
-         | Bet 
+         | Bet
          | Call
          | Check
-         | Fold  
+         | Fold
 
 ----- Update
 
 update : Msg -> Struc -> (Struc , Cmd Msg)
 update b s = case b of
-  Start ->   -- готовим ряд для формирования колоды
-        (s , Random.generate Initial (Random.list 2048 (Random.int 2 53)))
+  Start ->                                                  -- готовим ряд для формирования колоды
+        (s, Random.generate Initial (Random.list 2048 (Random.int 2 53)))
   Initial ys ->                    -- решаем, кто первый диллер ?
         let zs = mfun0 ys
         in case (List.take 1 zs) of
              [(s1,r1)] -> case (List.take 1 (List.drop 1 zs)) of
                 [(s2,r2)] -> if r1 > r2
                              then ( { s |
-                                      pdeal   = True , kdeal = False
-                                    , ppocket = [(s1,r1)]
-                                    , kpocket = [(s2,r2)]
-                                    , kstatus = Id
-                                    , pstatus = Id
-                                    , status  = Dc            
+                                      pdeal   = True      , kdeal = False
+                                    , ppocket = [(s1,r1)] , kpocket = [(s2,r2)]
+                                    , pstage  = Id        , kstage = Id
+                                    , status  = Dc
                                     } , Cmd.none )
                               else if r1 < r2
                                    then  ( { s |
-                                             pdeal   = False , kdeal = True
-                                           , ppocket = [(s1,r1)]
-                                           , kpocket = [(s2,r2)]
-                                           , kstatus = Id
-                                           , pstatus = Id
-                                           , status  = Dc            
+                                             pdeal   = False     , kdeal = True
+                                           , ppocket = [(s1,r1)] , kpocket = [(s2,r2)]
+                                           , pstage = Id         , kstage = Id
+                                           , status  = Dc
                                            } , Cmd.none)
                                     else ( { s |
-                                             pdeal   = True, kdeal = False
-                                           , ppocket = [(s1,r1)]
-                                           , kpocket = [(s2,r2)]
-                                           , kstatus = Id
-                                           , pstatus = Id
-                                           , status  = Dc            
+                                             pdeal   = True      , kdeal = False
+                                           , ppocket = [(s1,r1)] , kpocket = [(s2,r2)]
+                                           , pstage = Id         , kstage = Id
+                                           , status  = Dc
                                            } , Cmd.none)
-                _ -> (s, Cmd.none)            
+                _ -> (s, Cmd.none)
              _ -> (s , Cmd.none)
   Shuffle ->
         (s , Random.generate Hand (Random.list 2048 (Random.int 2 53)))
-  Hand ys ->  -- начало розыгрыша очередной раздачи
-        let zs = mfun0 ys  -- формируем колоду
+  Hand ys ->                                                -- начало розыгрыша каждой раздачи
+        let zs = mfun0 ys                                                 -- формируем колоду
         in ( { s |
                ppocket = List.take 2 zs                                   -- формируем руки
              , kpocket = List.take 2 (List.drop 2 zs)
              , board   = List.take 5 (List.drop 4 zs)                     -- формируем боард
              , pdeal   = if s.hand > 0 then not s.pdeal else s.pdeal
-             , kdeal   = if s.hand > 0 then not s.kdeal else s.kdeal                         
+             , kdeal   = if s.hand > 0 then not s.kdeal else s.kdeal
              , pot     = 3                                                -- анте
              , pstack  = if s.pdeal then s.pstack - 1 else s.pstack - 2
              , kstack  = if s.pdeal then s.pstack - 2 else s.kstack - 1
              , hand    = s.hand + 1                                       -- счетчик раздач
-             , pstatus = Ch , kstatus = Ch
+             , pstage = Ch , kstage = Ch, status = Pr
              } , Cmd.none)
   Fold ->  (s, Cmd.none)
-  Check -> (s, Cmd.none)          
+  Check -> (s, Cmd.none)
   Call ->  (s, Cmd.none)
   Bet  ->  (s, Cmd.none)
   AllIn ->
@@ -123,12 +119,12 @@ update b s = case b of
         then ( { s |
                  pot    = s.pot + s.pstack
                , pstack = s.pstack - s.kstack
-               , pstatus = Al
+               , pstage = Al
                } , Cmd.none)
          else ({ s |
                  pot    = s.pot + s.pstack
                , pstack = 0
-               , pstatus = Al
+               , pstage = Al
                } , Cmd.none)
 
 -- функция формирования колоды из ряда случайных чисел и хеша,
@@ -144,69 +140,118 @@ mfun0 ys = List.map (\k -> case (Dict.get k myhash52) of
 
 view : Struc -> Html Msg
 view m =
-   body [ style [("font-family","monospace") , ("background","green")] ]
-        [  div [style [("background","yellow")]]
-               [ p [style [("color","blue"),("font-size","12pt")]] [text "demo v0.00, MMXVII"] ]
-        ,  p  [] [] , br [] []
-        , div [style [("margin-left","200px")]] [tabls m, p [] [] ]
-        , p [style [("margin-top","20px")]] [text " "]    
+   body [ style [("font-family", "monospace")
+                , ("background", "green")]
+        ]
+        [ div [style [("background", "yellow")]]
+              [ p [style [("color","blue"),("font-size","12pt")]] [text "demo v0.00, MMXVII"] ]
+        , p [] [] , br [] []
+        , div [style [("margin-left", "200px")]]
+            [ tabls m
+            , p [] []
+            ]
+        , p [style [("margin-top", "20px")]] [text " "]
         , buttns m
         ]
 
-tigrok a b =
+tigrok m a b c =
     tr [style [("text-align","center")] ]
        (List.append
-         [td [style [("font-size","22pt"),("color","yellow"),("width" ,  "120px")], colspan 2]
-             [text ("stack $" ++ (toString a))]]
-         (List.map vfun0 b))
-
-tdeal m a =
+         [td [style [("font-size","22pt")
+                    ,("color","yellow")
+                    ,("width" ,  "110px")
+                    ], colspan 2
+             ]
+             [text ("stack $" ++ (toString a))]
+         ]
+         (if c == 1
+         then (List.map vfun0 b)
+         else if ( m.status == Pr )
+              then [ td [] [img [ src "img/ntycoon.png", height 110, width 80 ] [] ]
+                   , td [] [img [ src "img/ntycoon.png", height 110, width 80 ] [] ]
+                   ]
+              else (List.map vfun0 b)))
+             
+tdeal m a b =
    tr [] [ td [] [img [ src (if a && (m.status /= St || m.status /= Dc)
-                                 then "img/tycoonn.png"
-                                 else "img/green.png")
-                          , height 110 , width 80 ] [] ]                 
-         , td [style [("font-size","22pt"),("color","yellow"),("width" ,  "120px")]]
-              [ text (case m.kstatus of
-                                         Fo ->  "Fold"
-                                         Ch ->  "Check"
-                                         Ca ->  "Call"
-                                         Be ->  "Bet"
-                                         Al ->  "All in"               
-                                         _  ->  "Idle") ]]                     
+                             then "img/tycoonn.png"
+                             else "img/green.png"), height 110 , width 80 ] [] ]
+         , td [style [("font-size","22pt")
+                     ,("color","yellow")
+                     ,("width" ,  "110px")
+                     ]
+              ]
+              [ text (case b of
+                        Fo ->  "Fold"
+                        Ch ->  "Check"
+                        Ca ->  "Call"
+                        Be ->  "Bet"
+                        Al ->  "All in"
+                        _  ->  " ")
+              ]
+         ]
 
 tboard m =
-    tr [style [("text-align","center")]]
-       (List.append
-         (List.append    
-            [td [style [("font-size","22pt"),("color","yellow"),("width" ,  "120px")], colspan 2]
-                [text ("pot $" ++ (toString m.pot))]] 
-            (List.map vfun0
-                    (case m.status of
-                       Fl -> (List.take 3 m.board)
-                       Tn -> (List.take 4 m.board)
-                       Rv -> m.board
-                       _  -> []) ) )
-         (List.append
-            [td [] []]
-            [td [style [("font-size","22pt"),("color","yellow"),("width" ,  "120px")], colspan 2]
-                [text ("hand " ++ (toString m.hand))]] )
-       )
-       
-tabls : Struc -> Html a            
+  tr [style [("text-align","center")]]
+     [ td [style [("font-size","22pt"),("color","yellow"),("width" ,  "120px")], colspan 2]
+          [text ("pot $" ++ (toString m.pot))]  
+--     (case m.status of
+--             St ->
+                   , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+                   , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+                   , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+                   , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+                   , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]]
+         --     Dc ->
+         --           , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+         --           , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+         --           , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+         --           , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+         --           , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+         --     Fl ->
+         --           , vfun0 (List.take 1 m.board)
+         --           , vfun0 (List.take 1 (List.drop 1 m.board))
+         --           , vfun0 (List.take 1 (List.drop 2 m.board))
+         --           , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+         --           , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+         --     Tn ->
+         --           , vfun0 (List.take 1 m.board)
+         --           , vfun0 (List.take 1 (List.drop 1 m.board))
+         --           , vfun0 (List.take 1 (List.drop 2 m.board))
+         --           , vfun0 (List.take 1 (List.drop 3 m.board))
+         --           , td [] [img [ src "img/green.png", height 110, width 80 ] [] ]
+         --     Rv ->
+         --           , vfun0 (List.take 1 m.board)
+         --           , vfun0 (List.take 1 (List.drop 1 m.board))
+         --           , vfun0 (List.take 1 (List.drop 2 m.board))
+         --           , vfun0 (List.take 1 (List.drop 3 m.board))
+         --           , vfun0 (List.take 1 (List.drop 4 m.board))
+         --     _  ->
+         --           td [] []
+         -- )
+
+tabls : Struc -> Html a
 tabls m = table [style [("width", "920px")]]
-           [ tigrok m.kstack m.kpocket
-           , tdeal m m.kdeal
-           , tboard m
-           , tdeal m m.pdeal
-           , tigrok m.pstack m.ppocket
+           [ tigrok   m m.kstack m.kpocket 2
+           , tdeal    m m.kdeal m.kstage
+           , tboard   m
+           , tdeal    m m.pdeal m.pstage
+           , tigrok   m m.pstack m.ppocket 1
            ]
 
 vfun0 : (Int,Int) -> Html a
 vfun0 (s,r) =
   let ty b z x y =
-          td [style [("valign","top"), ("background",b), ("height","110px"), ("width","80px")]]
-             [span [style [("font-family","monospace"), ("color",z), ("font-size","24pt")]]
-                          [text (y ++ x)]]
+          td [style [("valign","top")
+                    , ("background",b)
+                    , ("height","110px")
+                    , ("width","80px")]
+             ]
+             [span [style [("font-family","monospace")
+                          , ("color",z)
+                          , ("font-size","24pt")]
+                   ]
+                   [text (y ++ x)]]
       r2 = case r of
              2  -> "2"
              3  -> "3"
@@ -223,38 +268,65 @@ vfun0 (s,r) =
              14 -> "A"
              _  -> ""
      in case s of
-          1 -> ty "white" "black" r2 "♠" 
-          2 -> ty "white" "black" r2 "♣" 
-          3 -> ty "white" "red"   r2 "♦" 
-          4 -> ty "white" "red"   r2 "♥" 
+          1 -> ty "white" "black" r2 "♠"
+          2 -> ty "white" "black" r2 "♣"
+          3 -> ty "white" "red"   r2 "♦"
+          4 -> ty "white" "red"   r2 "♥"
           0 -> ty "green" "green" "" ""
-          _ -> ty "green" "green" "" ""     
+          _ -> ty "green" "green" "" ""
 
 buttns : Struc -> Html Msg
 buttns m =
    let bstyle = [("width","70px"),("margin-left","30px")]
        cstyle = [("width","50px")]
-       dstyle = [("width","70px"),("margin-left","150px")]                
+       dstyle = [("width","70px"),("margin-left","150px")]
    in  div [style [("background","yellow")]]
-         [ button [ onClick AllIn, style bstyle, disabled (m.kstatus == Id) ] [ text " All In " ]
+         [ button [ onClick AllIn
+                  , style bstyle
+                  , disabled (m.kstage == Id)
+                  ] [ text " All In " ]
 
-         , button [ onClick Bet, style bstyle, disabled (m.kstatus == Id) ]  [ text " Bet " ]
-         , input  [ style cstyle, maxlength 3 , value (toString (2 * m.bet))] [ ]
-         , select [ style cstyle  ]  -- , Input Raise ]
+         , button [ onClick Bet
+                  , style bstyle
+                  , disabled (m.kstage == Id)
+                  ]  [ text " Bet " ]
+         , input  [ style cstyle
+                  , disabled  (m.kstage == Id)
+                  , maxlength 3
+                  , value (toString (2 * m.bet))
+                  ] [ ]
+         , select [ style cstyle
+                  , disabled  (m.kstage == Id)
+                  ]  -- , Input Raise ]
              [ option [value (toString (3 * m.bet))] [text (toString (3 * m.bet))]
              , option [value (toString (4 * m.bet))] [text (toString (4 * m.bet))]
              , option [value (toString (5 * m.bet))] [text (toString (5 * m.bet))]
              , option [value (toString (6 * m.bet))] [text (toString (6 * m.bet))]
              , option [value (toString (7 * m.bet))] [text (toString (7 * m.bet))]
-             , option [value (toString (8 * m.bet))] [text (toString (8 * m.bet))]                 
+             , option [value (toString (8 * m.bet))] [text (toString (8 * m.bet))]
              ]
 
-         , button [ onClick Call, disabled  (m.kstatus == Id), style bstyle ]  [ text " Call " ]
-         , button [ onClick Check, disabled (m.kstatus == Id), style bstyle ]  [ text " Check " ]
-         , button [ onClick Fold, disabled  (m.kstatus == Id), style bstyle ]  [ text " Fold  " ]
+         , button [ onClick Call
+                  , disabled  (m.kstage == Id)
+                  , style bstyle
+                  ]  [ text " Call " ]
+         , button [ onClick Check
+                  , disabled (m.kstage == Id)
+                  , style bstyle
+                  ]  [ text " Check " ]
+         , button [ onClick Fold
+                  , disabled  (m.kstage == Id)
+                  , style bstyle
+                  ]  [ text " Fold  " ]
 
-         , button [ onClick Shuffle, disabled (m.status == St), style dstyle ] [ text " Deal " ]
-         , button [ onClick Start   , style bstyle , disabled (m.status /= St) ] [ text " Start " ]
+         , button [ onClick Shuffle
+                  , disabled (m.status == St)
+                  , style dstyle
+                  ] [ text " Deal " ]
+         , button [ onClick Start
+                  , style bstyle
+                  , disabled (m.status /= St)
+                  ] [ text " Start " ]
          , br [] []
          ]
 
